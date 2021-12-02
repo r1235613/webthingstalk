61d4e8e9af1b83e17beeeaf98ed4bc5091bc8b1d
yuchingtw <yuchingchen.tw@gmail.com>
2021-12-03 00:05:41 +0800

    Add devices urls

--- webthing-talk/devices/urls.py
+++ webthing-talk/devices/urls.py
@@ -1,7 +1,17 @@
 from django.urls import include, path
 
+from xtalk_template.views import IndexView, AuthRedirectionView, AuthCallbackView, LogoutView
 from . import views
 
+app_name = 'xtalk_template'
+
 urlpatterns = [
-    path('', include('xtalk_template.urls', namespace='xtalk_template')),
+    path('', IndexView.as_view(), name='index'),
+    path('auth', AuthRedirectionView.as_view(), name='auth_redirect_endpoint'),
+    path(
+        'auth/callback',
+        AuthCallbackView.as_view(),
+        name='oauth2_redirect_endpoint'
+    ),
+    path('logout', LogoutView.as_view(), name='logout_endpoint'),
 ]
